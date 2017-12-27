e58019dcfa6d3d275391f0b630fda8cc46517bfd
Hernaldo González <hernaldog@gmail.com>
2017-12-27 16:15:09 -0300

    primera subida desde VS 2017

---
+++ Cap1/VS2017/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Bienvenidos a .NET Core en Visual Studio 2017!");
+            Console.ReadKey();
+        }
+    }
+}
