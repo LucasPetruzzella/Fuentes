4e9de56465eaeedfc36bccb9f6439aa279fe1c89
Hernaldo González <hernaldog@gmail.com>
2017-12-28 15:54:09 -0300

    primera subida de VS Code

---
+++ Cap1/VSCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 10; int y =0; int z = x/y;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
